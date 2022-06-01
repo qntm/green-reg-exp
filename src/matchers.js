9b463e001c1d07c1195e569c8ffcf0a6b077d2d5
qntm <qntm@users.noreply.github.com>
2022-06-01 21:07:31 +0100

    Update matchers.js

--- src/matchers.js
+++ src/matchers.js
@@ -1,9 +1,7 @@
-import { Matcher } from 'green-parse'
+import { UNICODE, or, fixed, seq, resolve, star, plus } from 'green-parse'
 import * as constructors from './constructors.js'
 import escapesBracket from './escapes-bracket.js'
 import escapesRegular from './escapes-regular.js'
-
-const { UNICODE, or, fixed, seq, resolve, star, plus } = Matcher
 
 // Non-special character, not escaped
 const matchNonEscapedBracketedChar = UNICODE
