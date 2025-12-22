c5afd763359f89ea95cf87860b4c86551485b21e
qntm <qntm@users.noreply.github.com>
2025-12-22 22:20:29 +0000

    Update de-anchor.test.js

--- test/de-anchor.test.js
+++ test/de-anchor.test.js
@@ -1,6 +1,5 @@
-/* eslint-env mocha */
-
 import assert from 'node:assert/strict'
+import { describe, it } from 'node:test'
 
 import { upliftAnchors, deAnchorConc, deAnchorPattern } from '../src/de-anchor.js'
 import matchers from '../src/matchers.js'
