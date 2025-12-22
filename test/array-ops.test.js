9c8c91497e3e32239ab410c94d92de6550969ea5
qntm <qntm@users.noreply.github.com>
2025-12-22 00:14:47 +0000

    Update array-ops.test.js

--- test/array-ops.test.js
+++ test/array-ops.test.js
@@ -1,48 +1,48 @@
 /* eslint-env mocha */
 
-import assert from 'assert'
+import assert from 'node:assert/strict'
 
 import { arrayOps } from '../src/array-ops.js'
 
 describe('arrayOps', () => {
   describe('and', () => {
     it('works', () => {
-      assert.deepStrictEqual(arrayOps.and([], []), [])
-      assert.deepStrictEqual(arrayOps.and(['a', 'b', 'c'], []), [])
-      assert.deepStrictEqual(arrayOps.and(['a', 'b', 'c'], ['b', 'c', 'd']), ['b', 'c'])
-      assert.deepStrictEqual(arrayOps.and(['a', 'b', 'c'], ['c', 'b', 'a']), ['a', 'b', 'c'])
+      assert.deepEqual(arrayOps.and([], []), [])
+      assert.deepEqual(arrayOps.and(['a', 'b', 'c'], []), [])
+      assert.deepEqual(arrayOps.and(['a', 'b', 'c'], ['b', 'c', 'd']), ['b', 'c'])
+      assert.deepEqual(arrayOps.and(['a', 'b', 'c'], ['c', 'b', 'a']), ['a', 'b', 'c'])
     })
   })
 
   describe('or', () => {
     it('works', () => {
-      assert.deepStrictEqual(arrayOps.or([], []), [])
-      assert.deepStrictEqual(arrayOps.or(['a', 'b', 'c'], ['a', 'b', 'c']), ['a', 'b', 'c'])
-      assert.deepStrictEqual(arrayOps.or(['a', 'b', 'c'], ['a', 'b', 'd']), ['a', 'b', 'c', 'd'])
+      assert.deepEqual(arrayOps.or([], []), [])
+      assert.deepEqual(arrayOps.or(['a', 'b', 'c'], ['a', 'b', 'c']), ['a', 'b', 'c'])
+      assert.deepEqual(arrayOps.or(['a', 'b', 'c'], ['a', 'b', 'd']), ['a', 'b', 'c', 'd'])
     })
   })
 
   describe('minus', () => {
     it('works', () => {
-      assert.deepStrictEqual(arrayOps.minus(['a', 'b', 'c'], []), ['a', 'b', 'c'])
-      assert.deepStrictEqual(arrayOps.minus(['a', 'b', 'c'], ['c', 'd', 'e']), ['a', 'b'])
+      assert.deepEqual(arrayOps.minus(['a', 'b', 'c'], []), ['a', 'b', 'c'])
+      assert.deepEqual(arrayOps.minus(['a', 'b', 'c'], ['c', 'd', 'e']), ['a', 'b'])
     })
   })
 
   describe('equal', () => {
     it('works', () => {
-      assert.strictEqual(arrayOps.equal(['a', 'b', 'c'], ['a', 'b']), false)
-      assert.strictEqual(arrayOps.equal(['a', 'b', 'c'], ['a', 'b', 'c']), true)
-      assert.strictEqual(arrayOps.equal(['a', 'b', 'c'], ['c', 'b', 'a']), true)
-      assert.strictEqual(arrayOps.equal(['a', 'b', 'c'], ['c', 'b', 'a', 'c']), true)
+      assert.equal(arrayOps.equal(['a', 'b', 'c'], ['a', 'b']), false)
+      assert.equal(arrayOps.equal(['a', 'b', 'c'], ['a', 'b', 'c']), true)
+      assert.equal(arrayOps.equal(['a', 'b', 'c'], ['c', 'b', 'a']), true)
+      assert.equal(arrayOps.equal(['a', 'b', 'c'], ['c', 'b', 'a', 'c']), true)
     })
   })
 
   describe('product', () => {
     it('works', () => {
-      assert.deepStrictEqual(arrayOps.product([]), [])
-      assert.deepStrictEqual(arrayOps.product(['a']), [['a']])
-      assert.deepStrictEqual(arrayOps.product(['a', 'b'], ['c', 'd', 'e']), [
+      assert.deepEqual(arrayOps.product([]), [])
+      assert.deepEqual(arrayOps.product(['a']), [['a']])
+      assert.deepEqual(arrayOps.product(['a', 'b'], ['c', 'd', 'e']), [
         ['a', 'c'],
         ['a', 'd'],
         ['a', 'e'],
