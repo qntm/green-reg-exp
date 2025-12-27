7e47cd373c3cd463d3c93a3d073c1adfe060be2f
qntm <qntm@users.noreply.github.com>
2025-12-27 23:43:58 +0000

    Update package.json (#47)
    * Update package.json
    
    * Weird that I have to specify test-coverage-exclude...
    
    * OK
    
    * There ya gpo

--- test/matches-empty-string.test.js
+++ test/matches-empty-string.test.js
@@ -4,11 +4,34 @@ import { describe, it } from 'node:test'
 import * as constructors from '../src/constructors.js'
 
 describe('matchesEmptyString', () => {
-  it('works', () => {
+  it('works for Concs', () => {
     assert.equal(new constructors.Conc([]).matchesEmptyString(), true)
+    assert.equal(new constructors.Conc([
+      new constructors.Term(
+        new constructors.Mult(
+          new constructors.Multiplicand(
+            new constructors.Charclass(['a'], false)
+          ),
+          new constructors.Multiplier(0, 3)
+        )
+      )
+    ]).matchesEmptyString(), true)
+    assert.equal(new constructors.Conc([
+      new constructors.Term(
+        new constructors.Mult(
+          new constructors.Multiplicand(
+            new constructors.Charclass(['a'], false)
+          ),
+          new constructors.Multiplier(1, 3)
+        )
+      )
+    ]).matchesEmptyString(), false)
   })
 
-  it('works', () => {
+  it('works for Patterns', () => {
     assert.equal(new constructors.Pattern([]).matchesEmptyString(), false)
+    assert.equal(new constructors.Pattern([
+      new constructors.Conc([])
+    ]).matchesEmptyString(), true)
   })
 })
